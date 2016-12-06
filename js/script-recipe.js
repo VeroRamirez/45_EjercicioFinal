ff935877af85dc6565f10fd027f563830a767630
VeroRamirez <veronica12.rp@gmail.com>
2016-12-06 15:20:05 -0600

    Etapa 10

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,13 +1,21 @@
+
+
 $(document).ready(function(){
     console.log("Lista pagina recipe.html");
     $('.js-menu').hide();
     
     $('.js-show-make').click(function(){
         $('.page').addClass('make');
+        $(this).addClass('active');
+        $('.js-show-recipe').removeClass('active');
     });
     
     $('.js-show-recipe').click(function(){
         $('.page').removeClass('make');
+        $(this).addClass('active');
+        $('.js-show-make').removeClass('active');
+    });
+    $('.js-back').click(function(){
+        window.location = 'index.html';      
     });
 });
-
